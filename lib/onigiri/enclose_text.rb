b9fa0de09d3721f0382d19b76072556c8739a17f
Slotos <slotos@ishtar.(none)>
2011-07-21 16:06:25 +0300

    Another segfault >.<

--- lib/onigiri/enclose_text.rb
+++ lib/onigiri/enclose_text.rb
@@ -3,19 +3,17 @@ module Onigiri
   register_handler :enclose_text
   class Document
     def enclose_text
-      # TODO: Beautify this without triggering another set of segfaults ._.
       dupe = dup
       wrapper = Onigiri::Document.parse('<p>').child
-      container = Onigiri::Document.parse('<body>')
-      dupe = container.child << dupe.dup.children if dupe.css('body').empty?
       body = dupe.css('body').children
       body = dupe.children if body.empty?
       body.each do |target|
         if target.parent && (target.text? || target.description.inline?)
-          wrap = target.replace(wrapper << target.dup)
+          wrap = target.add_previous_sibling(wrapper)
+          wrap << target.unlink
         end
       end
-      Onigiri::Document.parse dupe.children.to_s
+      dupe
     end
   end
 end
--- lib/onigiri/version.rb
+++ lib/onigiri/version.rb
@@ -1,3 +1,3 @@
 module Onigiri
-  VERSION = "0.1.5"
+  VERSION = "0.1.6"
 end
