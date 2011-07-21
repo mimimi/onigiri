d6d2da7577dc69dc2b9199bdaaac9fa3a0776915
Slotos <slotos@ishtar.(none)>
2011-07-21 15:08:51 +0300

    Work around issue #407 (https://github.com/tenderlove/nokogiri/issues/407)

--- lib/onigiri/enclose_text.rb
+++ lib/onigiri/enclose_text.rb
@@ -4,12 +4,12 @@ module Onigiri
   class Document
     def enclose_text
       dupe = dup
+      wrapper = Onigiri::Document.parse('<p>').child
       body = dupe.css('body').children
       body = dupe.children if body.empty?
       body.each do |target|
         if target.parent && (target.text? || target.description.inline?)
-          wrap = target.add_previous_sibling("<p>")[0]
-          wrap << target.unlink
+          wrap = target.replace(wrapper << target.dup)
         end
       end
       dupe
